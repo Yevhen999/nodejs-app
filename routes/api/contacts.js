3447a28c186874d9b1a3f969fe4404a1350c2720
Yevhen <leshchenkoyevhen@gmail.com>
2023-02-23 18:57:19 -0500

    add validate conditions

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -15,7 +15,7 @@ const { HttpError } = require("../../helpers");
 const router = express.Router();
 
 const addSchema = Joi.object({
-  name: Joi.string().required(),
+  name: Joi.string().alphanum().min(3).max(25).required(),
   email: Joi.string().required(),
   phone: Joi.string().required(),
 });
